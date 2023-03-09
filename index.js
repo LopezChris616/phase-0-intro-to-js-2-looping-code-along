1ab882935851e9633d2a38ce3f6d061b7b5317dc
Christian Lopez <lopezchristian1610@gmail.com>
2023-03-08 22:35:22 -0500

    all looping tests passed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(arr, name) {
+    const messageArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        messageArr.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`);
+    }
+    return messageArr;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10);
